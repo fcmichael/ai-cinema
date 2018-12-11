3e99351ae15eeb73971bb44342ea83e3af6463e2
fcmichael <michael.1@wp.pl>
2018-12-11 14:58:36 +0100

    Integrated angular with spring

--- frontend/src/main/web/src/app/app.module.ts
+++ frontend/src/main/web/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie/movie-list/movie-list.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/main/web/src/app/movie/movie.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Movie} from "./movie";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  private movieUrl: string = 'http://localhost:8080/movies';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getMovies(): Observable<Movie[]> {
+    return this.httpClient.get<Movie[]>(this.movieUrl);
+  }
+}
