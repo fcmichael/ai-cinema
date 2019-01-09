f63dcf987d47dd4cd9283a6218f2d4d165cf74f3
fcmichael <michael.1@wp.pl>
2019-01-09 14:26:13 +0100

    Block post and put at movies endpoint for unauthorized user

--- backend/src/main/java/pl/michalkruk/cinema/config/security/WebSecurity.java
+++ backend/src/main/java/pl/michalkruk/cinema/config/security/WebSecurity.java
@@ -1,11 +1,11 @@
 package pl.michalkruk.cinema.config.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -27,6 +27,10 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and()
                 .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/movies/**").authenticated()
+                .antMatchers(HttpMethod.PUT, "/movies/**").authenticated()
+                .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
                 .addFilter(new JWTAuthorizationFilter(authenticationManager()));
     }
